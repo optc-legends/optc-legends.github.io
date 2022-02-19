9bbe7eea0b949cb1dfe51282b1535105caf33bc8
antonlabz <20198370+antonlabz@users.noreply.github.com>
2022-02-19 13:44:45 +1100

    doffy v2 6+

--- js/scripts.js
+++ js/scripts.js
@@ -40,6 +40,7 @@ var base = [
   {base: 1832, evo: 2138},
   {base: 1869, evo: 2505},
   {base: 1883, evo: 3018},
+  {base: 1910, evo: 3550},
   {base: 1935, evo: 2300},
   {base: 1951, evo: 2830},
   {base: 2074, evo: 2363},
